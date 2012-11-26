76e30f08c05675b5d1abe2f3f026672a2de0e33d
Gilbert Corrales <gcorrales@gmail.com>
2012-11-26 00:10:01 -0600

    Adding licensing information.

--- src/pg.py
+++ src/pg.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+__author__ = "Gilbert Corrales (gcorrales@gmail.com)"
+__copyright__ = "Copyright (C) 2012 Gilbert Corrales"
+__license__ = "LGPL 3.0"
+__version__ = "0.1"
+
 import codecs
 import re
 import urllib2
